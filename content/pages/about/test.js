201832e9ed47476de5fcbf48a201bafaa1a79342
Slava 4REAL <slava@4real.io>
2020-02-11 02:06:51 -0500

    rm about query

--- content/pages/about/test.js
+++ content/pages/about/test.js
@@ -1,23 +1,23 @@
-import React, { Fragment } from "react";
-import { StaticQuery, graphql } from "gatsby";
+// import React, { Fragment } from "react";
+// import { StaticQuery, graphql } from "gatsby";
 
-const query = graphql`
-  query {
-    allGoogleSpreadsheetSheet2 {
-      edges {
-        node {
-          about
-        }
-      }
-    }
-  }
-`;
+// const query = graphql`
+//   query {
+//     allGoogleSpreadsheetSheet2 {
+//       edges {
+//         node {
+//           about
+//         }
+//       }
+//     }
+//   }
+// `;
 
-export default () => (
-  <StaticQuery
-    query={query}
-    render={data => (
-      <Fragment>{data.allGoogleSpreadsheetSheet2.edges[0].node.about}</Fragment>
-    )}
-  />
-);
+// export default () => (
+//   <StaticQuery
+//     query={query}
+//     render={data => (
+//       <Fragment>{data.allGoogleSpreadsheetSheet2.edges[0].node.about}</Fragment>
+//     )}
+//   />
+// );
