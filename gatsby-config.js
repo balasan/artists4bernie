7ba845d67f54ed1cbae0bf8eabf7ea3567bb2d5f
Slava 4REAL <slava@4real.io>
2020-02-11 00:41:21 -0500

    yaml

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
     siteTitle: `Artists4Bernie`,
     siteTitleAlt: `Artists4Bernie`,
     author: "DIS",
-    siteImage: "/banner.png",
+    siteImage: "/banner.jpg",
     siteHeadline: "Artists4Bernie",
     siteLanguage: "en",
     siteDescription:
       "Artists4Bernie is an artist-led campaign to endorse Bernie Sanders for President. Sign your name and join us to build a better future for our world.",
-    siteUrl: "https://www.artists4bernie.org"
+    siteUrl: "https://artists4bernie.org"
   },
   plugins: [
     {
@@ -43,24 +43,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Artists4Bernie`,
+        short_name: `Artists4Bernie`,
+        description: `Artists4Bernie is an artist-led campaign to endorse Bernie Sanders for President. Sign your name and join us to build a better future for our world.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
         icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`
-          }
+          // {
+          //   src: `/android-chrome-192x192.png`,
+          //   sizes: `192x192`,
+          //   type: `image/png`
+          // },
+          // {
+          //   src: `/android-chrome-512x512.png`,
+          //   sizes: `512x512`,
+          //   type: `image/png`
+          // }
         ]
       }
     },
